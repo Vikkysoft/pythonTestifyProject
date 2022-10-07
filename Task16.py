0189d5fdd7d3483afd7ecfd6818a55d860a00a52
Vikkysoft <bolvic28@gmail.com>
2022-10-07 11:11:55 +0100

    first commit

---
+++ Task16.py
@@ -0,0 +1,11 @@
+
+transportation = {
+    "land": "cars",
+    "air": "aeroplanes",
+    "water": "ships",
+    "wheels": [4, 2]
+}
+print("dictionary is:", transportation)
+
+transportation.update({"animals": "donkey", "Rail": "Train"})
+print("Update is:", transportation)
