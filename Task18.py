70d8c4968a781ed9246c569ccc96a5c6948d996a
Vikkysoft <bolvic28@gmail.com>
2022-10-12 12:17:56 +0100

    first commit

---
+++ Task18.py
@@ -0,0 +1,17 @@
+
+class Human:
+
+    leg_count = 4
+    can_walk = True
+
+    def __init__(self, name):
+        self.name = name
+
+
+Princess = Human("Princess")
+
+print(Princess.name)
+
+print(Princess.leg_count)
+
+print(Princess.can_walk)
